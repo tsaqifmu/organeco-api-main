7e25ddb101fda5d783f477174bcd70ca8e04b838
Tsaqif <mushoddaqt@gmail.com>
2023-05-27 06:32:20 +0700

    fix api response for predict

--- app/controller/predictController.js
+++ app/controller/predictController.js
@@ -1,8 +1,10 @@
 import httpStatus from "http-status";
 
+// import from models
 import Response from "../model/response.js";
 import Predict from "../model/predict.js";
 
+// import from utils
 import predictValidator from "../utils/predictValidator.js";
 
 const predict = async (req, res) => {
@@ -13,9 +15,8 @@ const predict = async (req, res) => {
 
     await predict.save();
 
-    response = new Response.Success(false, "berhasil masuk bos");
+    response = new Response.Success(false, "Data added successfully");
     return res.status(httpStatus.OK).json(response);
-    
   } catch (error) {
     response = new Response.Error(true, error.message);
     return res.status(httpStatus.BAD_REQUEST).json(response);
