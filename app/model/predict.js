e1fb341db127f597d2f433008981b252d9b81a96
Tsaqif Mushoddaq <91170683+tsaqifmu@users.noreply.github.com>
2023-06-12 09:39:27 +0700

    Update predict.js

--- app/model/predict.js
+++ app/model/predict.js
@@ -1,12 +1,32 @@
 import mongoose from "mongoose";
 
 const predictSchema = new mongoose.Schema({
-  ph: {
-    type: Number,
+  crop_type: {
+    type: String,
     required: true,
   },
   humidity: {
     type: Number,
+    required: true,
+  },
+  moisture: {
+    type: Number,
+    required: true,
+  },
+  nitrogen: {
+    type: Number,
+    required: true,
+  },
+  phosphorous: {
+    type: Number,
+    required: true,
+  },
+  potassium: {
+    type: Number,
+    required: true,
+  },
+  soil_type: {
+    type: String,
     required: true,
   },
   temperature: {
