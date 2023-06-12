715fa289a722903819123c8ec3550df5a7c1974d
Tsaqif Mushoddaq <91170683+tsaqifmu@users.noreply.github.com>
2023-06-12 09:39:12 +0700

    Update predictValidator.js

--- app/utils/predictValidator.js
+++ app/utils/predictValidator.js
@@ -1,9 +1,14 @@
 import Joi from "joi";
 
-const signInValidator = Joi.object({
-  ph: Joi.number().required(),
+const predictValidator = Joi.object({
+  crop_type: Joi.string().required(),
   humidity: Joi.number().required(),
+  moisture: Joi.number().required(),
+  nitrogen: Joi.number().required(),
+  phosphorous: Joi.number().required(),
+  potassium: Joi.number().required(),
+  soil_type: Joi.string().required(),
   temperature: Joi.number().required(),
 });
 
-export default signInValidator;
+export default predictValidator;
